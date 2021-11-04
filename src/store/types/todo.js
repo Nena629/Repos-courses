77b56b3a5abb31b468f73609ebe652be52247aae
Naima Attaf <naimattaf@gmail.com>
2021-11-04 11:11:33 +0100

    react-api-done

--- src/store/types/todo.js
+++
@@ -1,6 +0,0 @@
-export const TYPE_TODO_DELETE= "todo/delete"
-export const TYPE_TODO_ADD= "todo/add"
-export const TYPE_TODO_TOGGLE = "todo/toggle"
-export const TYPE_TODO_FILTER = "todo/filter"
-
-//eviter de faire une erreur quand on va les utiliser with uppercases and name of the action 
